672654295efe0cf0b2fabaa9b681a735912f0b4d
kaneganti <49714746+kaneganti@users.noreply.github.com>
2021-04-06 17:02:04 +0530

    Added even number functionality

--- src/main/java/com/myproj/EvenNumber.java
+++ src/main/java/com/myproj/EvenNumber.java
@@ -3,5 +3,8 @@ package com.myproj;
 public class EvenNumber {
     public void evenNum(){
         System.out.println("It evven number display functionality");
+        for(int ii=1;i<20;i++){
+            System.out.println("Number ::"+ii);
+        }
     }
 }
