f7644746149b5ef16dfb42a53a5948fb9ee5a3c0
kaneganti <49714746+kaneganti@users.noreply.github.com>
2021-04-06 17:04:08 +0530

    add odd number changes

--- src/main/java/com/myproj/OddNumber.java
+++ src/main/java/com/myproj/OddNumber.java
@@ -3,5 +3,8 @@ package com.myproj;
 public class OddNumber {
     public void oddNum(){
         System.out.println("Its odd number display functionality");
+        for(int j=1;j<=6;j++){
+            System.out.println("Odd number "+j);
+        }
     }
 }
