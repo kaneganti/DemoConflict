5c55e6677a903ae7bb085309307ff8f1eafae030
kaneg <kaneganti@gmail.com>
2021-04-06 17:22:00 +0530

    my first local commit

--- src/main/java/com/myproj/EvenNumber.java
+++ src/main/java/com/myproj/EvenNumber.java
@@ -2,11 +2,16 @@ package com.myproj;
 
 public class EvenNumber {
     public void evenNum(){
-        System.out.println("Even number test file ");
+        System.out.println("My Local test for even number");
+        for(int i=1;i<14;i++) {
+            System.out.println("Even number test file ");
+        }
         for(int i=1;i<12;i++){
             if(i % 2==0)
                 System.out.println("Its even number ::"+i);
         }
-        
+        for(int ii=1;ii<20;ii++){
+            System.out.println("Number ::"+ii);
+        }
     }
 }
--- src/main/java/com/myproj/OddNumber.java
+++ src/main/java/com/myproj/OddNumber.java
@@ -2,10 +2,16 @@ package com.myproj;
 
 public class OddNumber {
     public void oddNum(){
-         System.out.println("Odd number functionality test");
+        System.out.println("Odd test for my local changes");
+        for(int i=1;i<19;i++) {
+            System.out.println("Odd number functionality test");
+        }
         for(int i=1;i<13;i++){
             if(i % 2!=0)
                 System.out.println("Its ODD number ::"+i);
         }
+        for(int j=1;j<=6;j++){
+            System.out.println("Odd number test"+j);
         }
+    }
 }
