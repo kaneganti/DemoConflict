94d4a7c88bbd186ded36f2ea73a571ee73b88477
kaneg <kaneganti@gmail.com>
2021-04-06 16:57:04 +0530

    my first local commit

--- src/main/java/com/myproj/Demo.java
+++ src/main/java/com/myproj/Demo.java
@@ -4,13 +4,19 @@ public class Demo {
     public static void main(String[] args) {
         int num=17;
         boolean flag=false;
+        String name="Lakshmi";
+        String nameRev="";
+        for(int j=name.length();j>0;j--){
+            nameRev=nameRev+name.substring(j-1,j);
+        }
+        System.out.println("New Name ::"+nameRev);
             if(num==0 || num==1){
                 System.out.println(" Its Prime number"+num);
                 flag = true;
             }else {
                 for (int i = 2; i < num / 2; i++) {
                     if (num % i == 0) {
-                        System.out.println("Its nota prime number as its divisible by :")+i;
+                        System.out.println("Its nota prime number as its divisible by :"+i);
                         flag = true;
                         break;
                     }
@@ -18,8 +24,8 @@ public class Demo {
             }
         if(!flag)
             System.out.println("Prime number ::"+num);
-      int num=3;
-        for(int ii:num)
+      int var=3;
+        for(int ii=1;ii<=var;ii++)
             System.out.println("Integer value ::"+ii);
     }
 }
