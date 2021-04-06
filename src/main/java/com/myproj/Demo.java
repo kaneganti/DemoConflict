6176c3fa5f76249bd67c1000685d53a293b04396
kaneg <kaneganti@gmail.com>
2021-04-06 16:13:14 +0530

    my first local commit

--- src/main/java/com/myproj/Demo.java
+++ src/main/java/com/myproj/Demo.java
@@ -2,6 +2,23 @@ package com.myproj;
 
 public class Demo {
     public static void main(String[] args) {
+        int num=1;
+        boolean flag=false;
+
+            if(num==0 || num==1){
+                System.out.println(" Its Prime number"+num);
+                flag = true;
+            }else {
+                for (int i = 2; i < num / 2; i++) {
+                    if (num % i == 0) {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+        if(!flag)
+            System.out.println("Prime number ::"+num);
         System.out.println("my intellij proj");
+        System.out.println("my local changes applied");
     }
 }
