f5f986c0eb41bf51338587ffeeeaddfe66678203
kaneganti <49714746+kaneganti@users.noreply.github.com>
2021-04-06 15:56:24 +0530

    remote changes updated

--- src/main/java/com/myproj/Demo.java
+++ src/main/java/com/myproj/Demo.java
@@ -3,5 +3,7 @@ package com.myproj;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("my intellij proj");
+        System.out.println("My remote changes applies");
+        
     }
 }
